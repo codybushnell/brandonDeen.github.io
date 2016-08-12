e84c248b55e12ef3a851703989ccb61f4940f786
Brandon Deen <brandon.deen47@gmail.com>
2016-08-12 12:31:50 -0500

    added functionality to "More Projects" button, images for the additional projects shown

--- js/site.js
+++ js/site.js
@@ -15,6 +15,18 @@ $(document).ready(function(){
 					);
 				}
 			});
+
+	$("#show-more-projects").click(function(){
+		$("#more-projects").toggle();
+		$("#show-less-projects").toggle();
+		$("#show-more-projects").toggle();
+	});
+
+	$("#show-less-projects").click(function(){
+		$("#more-projects").toggle();
+		$("#show-less-projects").toggle();
+		$("#show-more-projects").toggle();
+	});
 });
 
 function animate_skills_bar(){
@@ -44,6 +56,9 @@ function get_page_sections(){
 
 	return sections;
 }
+
+
+
 
 //-------------------page content----------------------------------
 
@@ -159,7 +174,11 @@ function get_portfolio(){
 		+"<div class='row'>" 
 			+ projects.display_latest()
 		+"</div><br><br>"
-		+"<a type='button' target='_blank' class='btn btn-default' href='https://github.com/brandonDeen'>More Projects</a>"
+		+"<button type='button' id='show-more-projects' class='btn btn-default'>More Projects</button>"
+		+"<div id='more-projects' hidden='hidden'>"
+			+ projects.display_remaining()
+		+"</div>"
+		+"<button type='button' id='show-less-projects' class='btn btn-default' hidden='hidden'>Less Projects</button>"
 	;
 }
 
@@ -176,17 +195,17 @@ var bio = {
 				+"<img src='resources/sfa.jpg' data-toggle='tooltip' title='Stephen F. Austin State University'> B.S. Physics",
 	links: [
 		"&nbsp; <a href='mailto:brandon.deen47@gmail.com'><i class='fa fa-envelope'></i></a>", 
-		"&nbsp; <a href='https://github.com/brandonDeen'><i class='fa fa-github'></i></a>" , 
-		"&nbsp; <a href='https://www.linkedin.com/in/brandon-deen-116b7254'><i class='fa fa-linkedin'></i></a>" , 
-		"&nbsp; <a href='https://twitter.com/BruceWayne4Prez'><i class='fa fa-twitter'></i></a>" , 
+		"&nbsp; <a target='_blank' href='https://github.com/brandonDeen'><i class='fa fa-github'></i></a>" , 
+		"&nbsp; <a target='_blank' href='https://www.linkedin.com/in/brandon-deen-116b7254'><i class='fa fa-linkedin'></i></a>" , 
+		"&nbsp; <a target='_blank' href='https://twitter.com/BruceWayne4Prez'><i class='fa fa-twitter'></i></a>" , 
 		"&nbsp; <a href='resources/myResume.pdf' download='BrandonDeen_Resume'><i class='fa fa-file-pdf-o'></i></a>" 
 	],
 	big_links: [
 		"&nbsp; <a href='mailto:brandon.deen47@gmail.com'><i class='fa fa-5x fa-envelope'></i></a>"
-		,"&nbsp; <a href='https://github.com/brandonDeen'><i class='fa fa-5x fa-github'></i></a>" 
-		,"&nbsp; <a href='https://www.linkedin.com/in/brandon-deen-116b7254'><i class='fa fa-5x fa-linkedin'></i></a>" 
-		,"&nbsp; <a i href='https://twitter.com/BruceWayne4Prez'><i class='fa fa-5x fa-twitter'></i></a>" 
-		,"&nbsp; <a href='resources/myResume.pdf' download='BrandonDeen_Resume'><i class='fa fa-5x fa-file-pdf-o' data-toggle='tooltip' title='my resume (pdf)'></i></a>"
+		,"&nbsp; <a target='_blank' href='https://github.com/brandonDeen'><i class='fa fa-5x fa-github'></i></a>" 
+		,"&nbsp; <a target='_blank' href='https://www.linkedin.com/in/brandon-deen-116b7254'><i class='fa fa-5x fa-linkedin'></i></a>" 
+		,"&nbsp; <a target='_blank' href='https://twitter.com/BruceWayne4Prez'><i class='fa fa-5x fa-twitter'></i></a>" 
+		,"&nbsp; <a target='_blank' href='resources/myResume.pdf' download='BrandonDeen_Resume'><i class='fa fa-5x fa-file-pdf-o' data-toggle='tooltip' title='my resume (pdf)'></i></a>"
 	],
 	image: "resources/pic.jpg",
 	big_image: "resources/top-image.jpg",
@@ -195,7 +214,6 @@ var bio = {
 					+"<img class='card-img-top' src='"+ bio.image +"' alt='Brandon' width='255' height='255'>"      
 					+"<div class='card-block'>"  
 						+"<em class='card-title'>Hey, I'm "+ bio.name.split(' ')[0] +"</em>"                 
-					// +"</div>" 
 						+"<ul class='list-group list-group-flush dark'>"
 							+"<li class='list-group-item'>"+ bio.summary +"</li>"
 							+"<li class='list-group-item'>"+ bio.education +"</li>"
@@ -211,7 +229,7 @@ var skills = {
 	skills: [
 		{name: "Java", level: 70, level_str: "Proficient", color: "lightgreen" }
 		,{name: "Python", level: 70, level_str: "Proficient", color: "lightgreen" }
-		,{name: "C#", level: 70, level_str: "Proficient", color: "lightgreen" }
+		// ,{name: "C#", level: 70, level_str: "Proficient", color: "lightgreen" }
 		,{name: "SQL", level: 60, level_str: "Proficient", color: "lightgreen" }
 		,{name: "HTML", level: 60, level_str: "Proficient", color: "lightblue" }
 		,{name: "CSS", level: 50, level_str: "Familiar", color: "lightblue" }
@@ -222,7 +240,6 @@ var skills = {
 		skills_arr = skills.skills;
 		for(skill in skills_arr){
 			s += "<div class='skillbar clearfix' data-percent='"+ skills_arr[skill].level +"%'>"
-					// +"<div class='skillbar-title' style='background: "+ skills_arr[skill].color +";'>"
 					+"<div class='skillbar-title'>"
 						+"<span>"+	skills_arr[skill].name	+"</span>"
 					+"</div>"
@@ -236,91 +253,111 @@ var skills = {
 
 var projects = {
 		"projects":[
-			// {
-			// 	name: "Fidelity: Next Generation Call Center (NGCC) Migration", 
-			// 	description: "Migrating several Fidelity Client Service Teams, Business Units, and Portfolio Managers to the Next Generation Call Center using the Genesys Platform. Routing calls more accurately based on business rules. Javascript, scxml, Genesys", 
-			// 	github: "#", 
-			// 	image: "http://placehold.it/255x255"
-			// },
-			// {
-			// 	name: "Fidelity: Email Spoofer", 
-			// 	description: "An Email Spoofer used for testing email routing in Fidelity's Next Generation Call Center. C# (WPF & .NET)", 
-			// 	github: "#", 
-			// 	image: "http://placehold.it/255x255"
-			// },
-			// {
-			// 	name: "Fidelity: Email Test Automation", 
-			// 	description: "A step further from manual email spoofing for Call Center routing tests, using Ruby and Cucumber", 
-			// 	github: "#", 
-			// 	image: "http://placehold.it/255x255"
-			// },
-			// {
-			// 	name: "Fidelity: Genesys Rules Interpreter", 
-			// 	description: "Takes in a bulky and user unfriendly xml file and translates it into json and outputs the new business rules into a table with the ability to export to excel. C# (.NET & WPF), HTML, Javascript(JQuery), CSS(Bootstrap)", 
-			// 	github: "#", 
-			// 	image: "http://placehold.it/255x255"
-			// },
-			// {
-			// 	name: "Fidelity: Quote Audit Reporting Tool (QUARTz)", 
-			// 	description: "This tool allows analysts to run a wide variety of reports used in both stock exchange audits and market decisions. Java, Spring, SQL, HTML, CSS(Bootstrap), Javascript(JQuery)", 
-			// 	github: "#", 
-			// 	image: "http://placehold.it/255x255"
-			// },
-			// {
-			// 	name: "Fidelity: Buy Local Bonds", 
-			// 	description: "Fidelity's Leap Training Casestudy project. Java, Spring, SQL, HTML, CSS, Javascript", 
-			// 	github: "#", 
-			// 	image: "http://placehold.it/255x255"
-			// },
 			{
 				name: "PixelSense Transfer System", 
 				description: "The Lynx is a 2 way communication device using an Arduino and an array of LEDs, photodiodes, and a Nexus 5. The Lynx communicated with the Microsoft PixelSense Table, taking commands from both the PixelSense and the Nexus 7 on the Lynx. The demo application was a blackjack game.", 
 				github: "https://github.com/brandonDeen/DemoSoftware", 
-				technologies_used: "C#, Microsoft PixelSense SDK, Java, Android SDK",
+				technologies_used: "C#, Microsoft PixelSense SDK, Java, Android SDK, Visual Studio & Eclipse",
 				image: "resources/Argus.png"
 			},			
 			{
 				name: "pyRPG: A Tale in Text", 
 				description: "A text based role playing game, that runs in the console. You can create a character, customize their race, name and gender. Then go through a series of battles against tougher and tougher enemies, all while gaining gold that you can use to buy new equipment and upgrade you character with.", 
 				github: "https://github.com/brandonDeen/Text_RPG_Python", 
-				technologies_used: "Python, SQL, Future: Incorporate Flask to create a web project",
+				technologies_used: "Python, SQLite, Future: Incorporate Flask to create a web project",
 				image: "resources/console.jpg"
 			},
 			{
 				name: "DroidMD", 
 				description: "An Android application that allows the user to log their diet, add medications with reminders, and alerts the users family if a medication has been missed. Users can enter their doctors information, allowing their physician to get daily, weekly, or monthly updates on everything the user tracks", 
 				github: "#", 
-				technologies_used: "Java, Android SDK",
+				technologies_used: "Java, Android SDK, Eclipse",
 				image: "resources/droidmd.jpg"
 			},
 			{
 				name: "Angular Resume Page", 
 				description: "A resume page utilizing AngularJS. HTML, CSS, Javascript(AngularJS)", 
 				github: "https://github.com/brandonDeen/Resume_Angular_App", 
-				technologies_used: "",
-				image: "http://placehold.it/255x255"
+				technologies_used: "HTML, CSS - Bootstrap, Javascript - AngularJS, Sublime Text",
+				image: "resources/angular.png"
 			},
 			{
-				name: "Portfolio Page", 
-				description: "Portfolio page following the Udacity HTML & CSS course",
-				github: "https://github.com/brandonDeen/Portfolio_Page",
-				technologies_used: "",
-				image: "http://placehold.it/255x255"
+				name: "Fidelity: Next Generation Call Center (NGCC) Migration", 
+				description: "Migrating several Fidelity Client Service Teams, Business Units, and Portfolio Managers to the Next Generation Call Center using the Genesys Platform. Routing calls more accurately based on business rules.", 
+				technologies_used: "Javascript, Composer (Genesys Software Tool), AT&T Route It (Call Routing Tool), Eclipse", 
+				github: "#",
+				image: "resources/ngcc.jpg"
 			},
 			{
-				name: "Resume Page", 
-				description: "Resume page following the Udacity Javascript Beginner course",
-				github: "https://github.com/brandonDeen/frontend-nanodegree-resume",
-				technologies_used: "",
-				image: "http://placehold.it/255x255"
-			},			
+				name: "Fidelity: Email Spoofer", 
+				description: "An Email Spoofer used for testing email routing in Fidelity's Next Generation Call Center, using SMTP.", 
+				technologies_used: "C# - .NET & WPF, HTML, Javascript - JQuery, CSS - Bootstrap", 
+				github: "#",
+				image: "resources/spoofer.png"
+			},
+			{
+				name: "Fidelity: Genesys Rules Interpreter", 
+				description: "Takes in a bulky and user unfriendly xml file and translates it into json and outputs the new business rules into a table with the ability to export to excel.", 
+				technologies_used: "C# - .NET & WPF, HTML, Javascript - JQuery & Datatables, CSS - Bootstrap", 
+				github: "#",
+				image: "resources/rules.png"
+			},
+			{
+				name: "Fidelity: Quote Audit Reporting Tool (QUARTz)", 
+				description: "This tool allows analysts to run a wide variety of reports used in both stock exchange audits and market decisions. Java, Spring, SQL, HTML, CSS(Bootstrap), Javascript(JQuery)", 
+				technologies_used: "Java - Spring MVC & JDBC, HTML, CSS - Bootstrap, Javascript - JQuery & Datatables", 
+				github: "#",
+				image: "resources/quartz.jpg"
+			},
+			{
+				name: "Fidelity: Buy Local Bonds", 
+				description: "Fidelity's Leap Training Casestudy project. Java, Spring, SQL, HTML, CSS, Javascript", 
+				technologies_used: "Java - Spring MVC & iBatis, HTML, CSS - Bootstrap, Javascript - JQuery", 
+				github: "https://github.com/brandonDeen/brandonDeen.github.io",
+				image: "resources/blb.jpg"
+			},
+			{
+				name: "My Personal Webpage", 
+				description: "This page right here!", 
+				github: "#", 
+				technologies_used: "HTML, CSS - Bootstrap, FontAwesome, & W3 Schools, Javascript - JQuery",
+				image: "resources/personal.jpg"
+			},
 			{
 				name: "Android Calendar Application", 
 				description: "A simple calendar application that allows the user to create events, event reminders, and shared events. Java", 
 				github: "#", 
-				technologies_used: "",
-				image: "http://placehold.it/255x255"
+				technologies_used: "Java, Android SDK, Eclipse",
+				image: "resources/calendar.jpg"
 			},
+			{
+				name: "HackerRank", 
+				description: "My repo of code challenges & problems I do for fun.", 
+				github: "#", 
+				technologies_used: "Java, Javascript, Python",
+				image: "resources/hackerrank.png"
+			},
+			// {
+			// 	name: "Portfolio Page", 
+			// 	description: "Portfolio page following the Udacity HTML & CSS course",
+			// 	github: "https://github.com/brandonDeen/Portfolio_Page",
+			// 	technologies_used: "",
+			// 	image: "http://placehold.it/255x255"
+			// },
+			// {
+			// 	name: "Resume Page", 
+			// 	description: "Resume page following the Udacity Javascript Beginner course",
+			// 	github: "https://github.com/brandonDeen/frontend-nanodegree-resume",
+			// 	technologies_used: "",
+			// 	image: "http://placehold.it/255x255"
+			// },			
+			// {
+			// 	name: "Fidelity: Email Test Automation", 
+			// 	description: "A step further from manual email spoofing for Call Center routing tests.", 
+			// 	technologies_used: "Ruby, Cucumber, RubyMine", 
+			// 	github: "#",
+			// 	image: "http://placehold.it/255x255"
+			// },
 		],
 		
 		display_single: function(index){ 
@@ -342,12 +379,16 @@ var projects = {
 			}
 			return s.join('');
 		},
-		display_all: function(){
-			s = "";
-			for(project in projects.projects){
-
+		display_remaining: function(){
+			s = [];
+			for(var i=3; i<projects.projects.length; i+=3){
+				s.push("<br><div class='row'>");
+				s.push(projects.display_single(i));
+				s.push(projects.display_single(i+1));
+				s.push(projects.display_single(i+2));
+				s.push("</div>");
 			}
-			return s;
+			return s.join('');
 		}
 
 	};
